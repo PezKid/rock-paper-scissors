35a76d2aa8eb28c326810461d3826ab73cda844f
Rylan Dalpez <rylandalpez@Rylans-MacBook-Air-2.local>
2024-09-10 15:29:34 -0700

    Add UI and refactor game code to work with UI.

--- script.js
+++ script.js
@@ -10,66 +10,80 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let inputting = true;
-    let input = "n/a";
-    while (inputting) {
-        input = prompt("Enter input 'rock', 'paper', or 'scissors'").toLowerCase();
-        if (input === "rock" || input === "paper" || input === "scissors")
-            inputting = false;
-    }
-    return "" + input;
-}
-
-function playGame(numRounds) {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-        let message = "";
-        
-        if (humanChoice === computerChoice) {
-            message = "You tied!"
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            message = "You win! Rock beats Scissors";
-            humanScore ++;
-        } else if (humanChoice === "rock" && computerChoice === "paper") {
-            message = "You lose. Paper beats Rock";
-            computerScore ++;
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            message = "You win! Paper beats Rock";
-            humanScore ++;
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            message = "You lose. Scissors beats Paper";
-            computerScore ++;
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            message = "You win! Scissors beats Paper";
-            humanScore ++;
-        } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            message = "You lose. Rock beats Scissors";
-            computerScore ++;
-        } else {
-            message = "improper inputs";
-        }
-
-        console.log(message);
-    }
-
-    while (numRounds > 0) {
-        playRound(getHumanChoice(), getComputerChoice());
-        numRounds --;
-    }
-
-    let winMessage = "";
-    if (humanScore > computerScore) {
-        winMessage = `You won ${humanScore}-${computerScore}!`;
-    } else if (humanScore < computerScore) {
-        winMessage = `You lost ${humanScore}-${computerScore}.`;
+function playRound(humanChoice, computerChoice) {
+    let message = "";
+    
+    if (humanChoice === computerChoice) {
+        message = "You tied!"
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        message = "You win! Rock beats Scissors";
+        humanScore ++;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        message = "You lose. Paper beats Rock";
+        computerScore ++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        message = "You win! Paper beats Rock";
+        humanScore ++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        message = "You lose. Scissors beats Paper";
+        computerScore ++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        message = "You win! Scissors beats Paper";
+        humanScore ++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        message = "You lose. Rock beats Scissors";
+        computerScore ++;
     } else {
-        winMessage = `You tied ${humanScore}-${computerScore}.`;
+        message = "improper inputs";
     }
 
-    console.log("Game Over!\n" + winMessage);
+    messageDisplay.textContent = message;
+    scoreDisplay.textContent = humanScore + "-" + computerScore;
+    
+    if (humanScore == winScore) {
+        messageDisplay.textContent = "Game Over, You Win!";
+        doEndState();
+    } else if (computerScore == winScore) {
+        messageDisplay.textContent = "Game Over, You Lose";
+        doEndState();
+    }
 }
 
-playGame(5);
+function doEndState() {
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+
+    const restartButton = document.createElement("button");
+    restartButton.textContent = "Restart";
+    restartButton.addEventListener("click", () => {
+        restartGame();
+        restartButton.remove();
+    });
+    container.appendChild(restartButton);
+}
+
+function restartGame() {
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorsButton.disabled = false;
+    humanScore = 0;
+    computerScore = 0;
+    messageDisplay.textContent = "";
+    scoreDisplay.textContent = "";
+}
+
+const container = document.querySelector("#container");
+const rockButton = document.querySelector("#rock-button");
+const paperButton = document.querySelector("#paper-button");
+const scissorsButton = document.querySelector("#scissors-button");
+const messageDisplay = document.querySelector("#message-display");
+const scoreDisplay = document.querySelector("#score-display");
+
+rockButton.addEventListener("click", () => playRound("rock", getComputerChoice()));
+paperButton.addEventListener("click", () => playRound("paper", getComputerChoice()));
+scissorsButton.addEventListener("click", () => playRound("scissors", getComputerChoice()));
+
+let humanScore = 0;
+let computerScore = 0;
+const winScore = 5;
